8fe09a0d5ab5ddbdaf777bd0ed95df8ebc9aa775
namkyung.lee <iddbxk@daum.net>
2023-12-20 20:31:48 +0900

    install: jest-junit & github actions ci flow for testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
-describe('<App />', () => {
+describe('App Component', () => {
   it('should render the App', () => {
     const { container, getByText } = render(<App />);
 
