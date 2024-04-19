0bb28d1affc6ebf589b341fa3ce0df6c2033bedc
poqopo <block.poqopo@gmail.com>
2024-04-19 14:20:20 +0900

    Add Nonce Test

--- src/utils/mintNFT.ts
+++ src/utils/mintNFT.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'api.myyonseinft.com'; // prepare url
-const META_BASE_URL = 'https://yonseinft.s3.amazonaws.com/MAJOR';
+const API_URL = 'https://api.myyonseinft.com/makeNFT'; // prepare url
+const META_BASE_URL = 'https://myyonseinft.s3.amazonaws.com/MAJOR';
 
 function getRandomInt(max: number) {
   return Math.floor(Math.random() * max) + 1;
@@ -31,4 +31,21 @@ export function mintNFT(address: string | undefined, tokenuri: string) {
     return false;
   }
   return true;
+}
+
+export function TEST() {
+  try {
+    for (let i = 0; i < 30; i += 1) {
+      console.log(i);
+      axios
+        .get(
+          'https://api.myyonseinft.com/makeNFT?tokenuri=https://yonseinft.s3.amazonaws.com/MAJOR/TEST/json/1.json&address=0x0380bb57500562DA6Da4789AE9D04669c28243F1',
+        )
+        .then((response) => {
+          return response.data;
+        });
+    }
+  } catch (e) {
+    return e;
+  }
 }
