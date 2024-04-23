98fb17188097ad3e9632c118e6b97903ffbdf566
poqopo <block.poqopo@gmail.com>
2024-04-23 13:52:30 +0900

    Add Test

--- src/pages/QnA.tsx
+++ src/pages/QnA.tsx
@@ -1,6 +1,6 @@
 export default function QnA() {
   return (
-    <div className="h-full flex flex-col place-content-center pb-10">
+    <div className="h-full flex flex-col place-content-center pb-[80px]">
       <h3 className="mt-9 font-bold text-[20px]">Q & A</h3>
       <div className="w-4/5 m-auto text-start">
         <h2 className="mt-10 mb-5 font-bold text-[15px]">
